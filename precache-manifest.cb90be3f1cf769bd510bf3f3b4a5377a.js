5897be9acea575304a39bdf840ba2a1321ff4c10
Christopher Meza <m4tchb0x87@gmail.com>
2019-01-08 19:28:06 -0600

    Updates

---
+++ precache-manifest.cb90be3f1cf769bd510bf3f3b4a5377a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cafc84072ecfa75847c7",
+    "url": "/Marvel-Memory/static/js/runtime~main.cafc8407.js"
+  },
+  {
+    "revision": "e7dbf0c2dab3abfd1916",
+    "url": "/Marvel-Memory/static/js/main.e7dbf0c2.chunk.js"
+  },
+  {
+    "revision": "9b8a12edac4f94a5a0a7",
+    "url": "/Marvel-Memory/static/js/1.9b8a12ed.chunk.js"
+  },
+  {
+    "revision": "e7dbf0c2dab3abfd1916",
+    "url": "/Marvel-Memory/static/css/main.e536a639.chunk.css"
+  },
+  {
+    "revision": "9b8a12edac4f94a5a0a7",
+    "url": "/Marvel-Memory/static/css/1.c0933b40.chunk.css"
+  },
+  {
+    "revision": "cc6bf244f160995218ad701dc3f93b81",
+    "url": "/Marvel-Memory/index.html"
+  }
+];
